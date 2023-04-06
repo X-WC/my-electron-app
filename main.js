b8199e5f39764e866c275bf98bf8bb1bb926a426
聪聪不是冲冲 <862348941@qq.com>
2023-04-06 21:43:55 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const {app, BrowserWindow, ipcMain} = require('electron');
+const path = require('path');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+        }
+    })
+
+    // ipc通信
+    ipcMain.handle('ping', ()=> 'pong');
+
+    win.loadFile('index.html').then();
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    })
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+})
+
+console.log(process.platform); // darwin
+
+
